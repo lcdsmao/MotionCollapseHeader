70b3e79f8369fc59f924a176adfc08bd43c61c6c
mao <lcdsmao@gmail.com>
2019-04-16 23:54:52 +0900

    Add collapse header

--- app/src/main/java/com/paranoid/mao/motioncollapseheader/MainActivity.kt
+++ app/src/main/java/com/paranoid/mao/motioncollapseheader/MainActivity.kt
@@ -2,11 +2,38 @@ package com.paranoid.mao.motioncollapseheader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = List(30) { "Dummy Items" }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = Adapter()
+        recyclerView.setHasFixedSize(true)
     }
+
+    inner class Adapter : RecyclerView.Adapter<ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun getItemCount(): Int = items.size
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        }
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
